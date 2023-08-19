e32165ed0f86039d95f995d9f8714e3e9d700e41
CrioUser <crio-user@criodo.com>
2023-08-19 14:00:11 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,5 @@
 
-const config = { backendEndpoint: "http://65.0.40.89:8082" };
+// const config = { backendEndpoint: "http://65.0.40.89:8082" };
+const config = { backendEndpoint: "http://13.127.204.148:8082" };
 
 export default config;
